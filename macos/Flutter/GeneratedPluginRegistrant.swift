55df7da9fdf9b6cc75586f9b041fcae8591bf34f
Jose Eduardo Rojas Jimenez <joseeduardox@gmail.com>
2026-09-03 23:35:43 -0600

    chore(platform): upgrade dart_ipfs and flutter_secure_storage, deintegrate CocoaPods
    Pin dart_ipfs to ^1.12.0 and flutter_secure_storage to ^10.3.1. Switch
    the macOS runner to Swift Package Manager by deintegrating CocoaPods,
    removing the Podfile and xcconfig Pod includes, and recreating
    Runner.xcworkspace. Update generated plugin registrants for SPM.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,10 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
-import flutter_secure_storage_macos
+import flutter_secure_storage_darwin
 import local_auth_darwin
 import package_info_plus
-import path_provider_foundation
 import sentry_flutter
 import shared_preferences_foundation
 import sqflite_darwin
@@ -17,10 +16,9 @@ import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
